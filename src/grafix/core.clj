dabb273a2f888949355e1119ac92233eb581456b
unknown <gaffl@ONE7-FAL136086.au.fjanz.com>
2014-07-26 13:46:58 +1000

    Rewrite to use lwjgl example instead

--- src/grafix/core.clj
+++ src/grafix/core.clj
@@ -4,39 +4,30 @@
            (org.lwjgl LWJGLException)))
 
 
-(defn init-window
-  [width height title]
-  (def globals (ref {:width width
-                     :height height
-                     :title title
-                     :angle 0.0
-                     :last-time (System/currentTimeMillis)}))
+(defn init-gl []
+  (GL11/glClearColor 0.0 0.5 0.5 0.0)
+  )
+
+(defn render-gl []
+  (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
+  ;; Render GL here
+  )
+
+(defn run []
   (try
-    (Display/setDisplayMode (DisplayMode. width height))
-    (Display/setTitle title)
-    (Display/create)
-    (catch LWJGLException e (.getMessage e))))
-
-(defn init-gl
-  []
-  (println "OpenGL version:" (GL11/glGetString GL11/GL_VERSION))
-  (GL11/glClearColor 0.0 0.0 0.0 0.0)
-  (GL11/glMatrixMode GL11/GL_PROJECTION)
-  (GLU/gluOrtho2D 0.0 (:width @globals)
-                  0.0 (:height @globals))
-  (GL11/glMatrixMode GL11/GL_MODELVIEW))
-
-(defn run
-  []
-  (init-window 800 600 "Grafix demos")
+    (do
+      (Display/setDisplayMode (DisplayMode. 800 600))
+      (Display/setTitle "Hello GL")
+      (Display/create))
+    (catch LWJGLException e ""))
   (init-gl)
   (while (not (Display/isCloseRequested))
-    ;; TODO: Display update goes here.
-    (Display/update)
-    (Display/sync 60))
+    (render-gl)
+    (Display/update))
   (Display/destroy))
 
 (defn -main [& args]
   (println "OpenGL demonstration and tutorials")
-  (run))
+  (.start (Thread. run)))
 
+(.start (Thread. run))
